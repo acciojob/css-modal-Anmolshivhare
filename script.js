e90867353120faf79edbf9a6cc9d48588cb34820
Anmolshivhare <101089259+Anmolshivhare@users.noreply.github.com>
2024-04-27 21:00:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.getElementById("openModal").addEventListener("click", function() {
+  document.getElementById("modal").style.display = "block";
+});
+
+document.querySelector(".modal").addEventListener("click", function(event) {
+  if (event.target === this || event.target.classList.contains("close-modal")) {
+    this.style.display = "none";
+  }
+});
